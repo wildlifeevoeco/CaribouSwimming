42be36b7fe106338eae2736efb5bfda7872809d4
qwebber <webber.quinn@gmail.com>
2019-07-03 20:38:39 -0230

    blah trying to get osm to work - failure!

--- scripts/2-map.R
+++ scripts/2-map.R
@@ -4,18 +4,38 @@ library(ggplot2)
 library(osmdata)
 
 
+
+remotes::install_github('ropensci/osmdata')
+
+q <- opq(bbox = c(-54.35, 49.53, -53.9, 49.755))
+
+q <- opq(bbox = 'Fogo')
+
+
+
 FogoCaribou <- FogoCaribou[EASTING > 650000 & EASTING < 800000 &
                              NORTHING > 5470000  & NORTHING < 5520000]
 
 bbox_to_string(getbb, "")
 
-fogo <- st_read(some shapefile)
-islands <- gDissolve(to separate islands)
+Fogo <- st_read("/Users/quinnwebber/Dropbox/Fogo/RSF_for_app/Code and files/FogoPoly.shp")
+
+
+plot(Fogo)
+
+st_area(Fogo)
+
+islands <- gUnaryUnion(Fogo)
 islands$area <- gArea(islands, byid = TRUE)
 sub <- islands[islands$area > 10km2]
 
 ## Map 
-osmdata_sf()
+## Not run: 
+NL_sf <- opq("Newfoundland") %>%
+  #add_osm_feature(key="roads", value="ruins") %>%
+  osmdata_sf()
+
+ggplot() + geom_sf(data = fortify(NL_sf))
 
 ggplot(sub) +
   geom_sf() +
