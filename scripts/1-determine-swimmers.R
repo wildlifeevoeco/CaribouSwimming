0dad62ae03879528d3d1b508c5d222b80caa2488
Quinn Webber <webber.quinn@gmail.com>
2019-12-16 16:22:23 -0330

    Update 1-determine-swimmers.R

--- scripts/1-determine-swimmers.R
+++ scripts/1-determine-swimmers.R
@@ -1,10 +1,22 @@
+
+library(sf)
+library(sp)
 library(data.table)
 library(rgdal)
 library(ggplot2)
 library(maptools)
-library(osmdata)
 library(dplyr)
-
+library(data.table)
+library(plotKML)
+library(ggmap)
+library(osmdata)
+library(cartography)
+library(raster)
+library(prettymapr)
+library(rosm)
+library(stars)
+library(mapview)
+library(fasterize)
 
 caribou <- fread('input/FogoCaribou.csv')
 ## Loc fields
@@ -16,24 +28,95 @@ caribou[, c('EASTING', 'NORTHING') := as.data.table(project(cbind(X_COORD, Y_COO
 caribou <- caribou[EASTING > 690000 & EASTING < 800000 &
                              NORTHING > 5470000  & NORTHING < 5520000]
 
-## load Fogo shapefile
-Fogo <- readShapeSpatial("/Users/quinnwebber/Dropbox/Fogo/RSF_for_app/Code and files/FogoPoly.shp")
-
 caribou <- caribou[JDate > 90 & JDate < 330]
 
 ##
-FO2016011 <- caribou[ANIMAL_ID == "FO2016011"]
-FO2017001 <- caribou[ANIMAL_ID == "FO2017001"]
-FO2017013 <- caribou[ANIMAL_ID == "FO2017013"]
+swimmers <- caribou[ANIMAL_ID == "FO2016011" |
+                     ANIMAL_ID == "FO2017001" |
+                     ANIMAL_ID == "FO2017013"]
+
+ns <- makebbox(49.0, -54.0, 50.0, -55.0)
+ns <- makebbox(max(swimmers[ANIMAL_ID == "FO2016011"]$Y_COORD), 
+               max(swimmers[ANIMAL_ID == "FO2016011"]$X_COORD),
+               min(swimmers[ANIMAL_ID == "FO2016011"]$Y_COORD), 
+               min(swimmers[ANIMAL_ID == "FO2016011"]$X_COORD))
+
+ggplot(swimmers) +
+  geom_point(aes(X_COORD, Y_COORD))
 
 
-ggplot(Fogo) + 
-  geom_polygon(aes(long,lat,group=group)) +
-  #ylim(5490000, 5500000) +
-  #ylim(5490000, 5494000) +
-  #xlim(690000,705000) +
-  geom_point(data = FO2016011, aes(EASTING, NORTHING, color = ANIMAL_ID),
-             alpha =0.5) +
-  geom_path(data = FO2016011, aes(EASTING, NORTHING, color = ANIMAL_ID),
-            color = "black", alpha = 0.1)
+### extract islands from osm 
+
+coords <- makebbox(49.5194, -54.1878, 
+                   49.65, -54.3533)
+
+coordsOSM <- osm.raster(coords, projection=CRS('+proj=utm +zone=21 ellps=WGS84'), crop=TRUE)
+coordsOSM2 <- coordsOSM[[1]] != 170
+coordsOSM2[coordsOSM2 == 0] <- NA
+coordsOSM3 <- st_as_stars(coordsOSM2) %>% 
+  st_as_sf(as_points = FALSE, use_integer = TRUE, merge = TRUE, na.rm = TRUE)
+
+coordsOSM3$island <- seq_along(coordsOSM3$geometry)
+
+r <- raster(ymn = st_bbox(coordsOSM3)$ymin,
+            ymx = st_bbox(coordsOSM3)$ymax, 
+            xmn = st_bbox(coordsOSM3)$xmin,
+            xmx = st_bbox(coordsOSM3)$xmax, 
+            crs = CRS('+proj=utm +zone=21 ellps=WGS84'))
+r <- fasterize(coordsOSM3, r, field = "island")
+
+swimmers[, islands := extract(r, matrix(c(EASTING, NORTHING), ncol = 2))]
+
+
+swimmers[, .N, by = "islands"]
+
+plot(r, xlim = c(685000, 710000), 
+     ylim = c(5490000, 5502000))
+par(new = T)
+plot(NORTHING ~ EASTING, data = swimmers[islands == 71], 
+     xlim = c(685000, 710000), 
+     ylim = c(5490000, 5502000))
+
+## rename islands
+swimmers <- swimmers[!is.na(islands),]
+ 
+swimmers$islands2[swimmers$islands == 43] <- "Fogo"
+swimmers$islands2[swimmers$islands == 53] <- "North Long"
+swimmers$islands2[swimmers$islands == 55] <- "North Long"
+swimmers$islands2[swimmers$islands == 58] <- "Blundon"
+swimmers$islands2[swimmers$islands == 67] <- "Brother"
+swimmers$islands2[swimmers$islands == 68] <- "W. Indian"
+swimmers$islands2[swimmers$islands == 70] <- "South Long"
+swimmers$islands2[swimmers$islands == 71] <- "E. Indian"
+swimmers$islands2[swimmers$islands == 74] <- "Kate"
+
+
+swimmers[, difference := data.table::shift(islands, type = "lead") - islands, by = .(ANIMAL_ID, Year)]
+
+
+swimmers$islands3[swimmers$difference == -10] <- "Blundon"
+swimmers$islands3[swimmers$difference == 3] <- "E. Indian"
+swimmers$islands3[swimmers$difference == -3] <- "W. Indian"
+swimmers$islands3[swimmers$difference == -1] <- "Brother"
+swimmers$islands3[swimmers$difference == 1] <- "W. Indian"
+swimmers$islands3[swimmers$islands2 == "Blundon" & 
+                       swimmers$difference == 13] <- "E. Indian"
+swimmers$islands3[swimmers$islands2 == "North Long" & 
+                       swimmers$difference == 13] <- "W. Indian"
+swimmers$islands3[swimmers$difference == -13] <- "North Long"
+swimmers$islands3[swimmers$difference == -25] <- "Fogo"
+swimmers$islands3[swimmers$difference == 25] <- "W. Indian"
+swimmers$islands3[swimmers$difference == -15] <- "North Long"
+swimmers$islands3[swimmers$difference == 15] <- "W. Indian"
+swimmers$islands3[swimmers$difference == -6] <- "W. Indian"
+swimmers$islands3[swimmers$difference == 6] <- "Kate"
+swimmers$islands3[swimmers$difference == 2] <- "South Long"
+swimmers$islands3[swimmers$difference == -2] <- "W. Indian"
+
+swimmers2 <- swimmers[difference !=0]
+swimmers2$swimDir <- paste(swimmers2$islands2, swimmers2$islands3, sep = "_")
+
+## summary stats
+
+swimmers2[, .N, by = c("ANIMAL_ID", "swimDir")]
 
