b4cd9b76781f4d0a3b30f6ec658911287cafd4fc
Alec L. Robitaille <robit.alec@gmail.com>
2020-02-24 14:54:04 -0330

    add export region

--- scripts/1-extract-islands.js
+++ scripts/1-extract-islands.js
@@ -1,4 +1,4 @@
-// Extract islands with SENTINEL - 2
+// Extract islands with SENTINEL2
 // Alec Robitaille
 
 var geometry = ee.Geometry.Polygon(
@@ -9,33 +9,34 @@ var geometry = ee.Geometry.Polygon(
           
 var col = ee.ImageCollection('COPERNICUS/S2')
   .filterDate('2019-07-01', '2019-10-01')
-  .filterBounds(geometry)
+  .filterBounds(geometry);
   
 var value = function(img) {
   return(img.addBands(img.divide(10000)
                          .select('B4', 'B3', 'B2')
                          .rgbToHsv()
-                         .select('value')))
-}
+                         .select('value')));
+};
 
 var dark = function(img) {
-  return(img.addBands(ee.Image(1).divide(img.select('value')).rename(['dark'])))
-}
+  return(img.addBands(ee.Image(1).divide(img.select('value')).rename(['dark'])));
+};
 
 var qual = col.map(value)
               .map(dark)
-              .qualityMosaic('dark')
+              .qualityMosaic('dark');
 
-var land = qual.select('B8').gt(350)
+var land = qual.select('B8').gt(350);
 
-Map.addLayer(col)
-Map.addLayer(qual)
-Map.addLayer(land)
+Map.addLayer(col);
+Map.addLayer(qual);
+Map.addLayer(land);
 
 
 Export.image.toDrive({
   image: land,
   description: 'caribouswimming',
   scale: 20,
-  fileNamePrefix: 'fogo-land-caribou-swimming'
-})
+  fileNamePrefix: 'fogo-land-caribou-swimming',
+  region: geometry
+});
