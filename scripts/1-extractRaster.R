3890012d675c44a96fc5a5aeee9b6735a5a0920f
Alec L. Robitaille <robit.alec@gmail.com>
2020-02-26 14:50:37 -0400

    better comments

--- scripts/1-extractRaster.R
+++ scripts/1-extractRaster.R
@@ -1,4 +1,4 @@
-### Extract OSM data ====
+### Extract islands from OSM ====
 # Quinn M.R. Webber, Jack G. Hendrix, Alec L. Robitaille,  Eric Vander Wal
 
 
@@ -8,52 +8,52 @@ libs <- c('data.table',
           'raster',
           'sf',
           'osmdata',
-          'stars',
           'fasterize')
 lapply(libs, require, character.only = TRUE)
 
-### Extract islands from OSM ----
-# Set up bounding box - xmin, ymin, xmax, ymax
+### Download OSM data ----
+# Set up bounding box - order: xmin, ymin, xmax, ymax
 bb <- c(xmin = -54.3533,
         ymin = 49.5194,
         xmax = -53.954220,
         ymax = 49.763834)
 
-# Projections
-latlon <- st_crs(4326)
-utm <- CRS('+proj=utm +zone=21 ellps=WGS84')
-
-# Download osm in bbox as raster stack
-# place, location, 
+# Download osm coastlines in bbox
 zz <- opq(bb) %>% 
   add_osm_feature(key = 'natural', value = 'coastline') %>% 
   osmdata_sf()
 
+# Grab polygons (small islands)
 polys <- zz$osm_polygons
+
+# Grab lines (large islands including Fogo)
 lns <- zz$osm_lines
 
+# Union -> polygonize -> cast lines = geo set
 castpolys <- st_cast(st_polygonize(st_union(lns)))
 
+# Combine geometries and cast as sf
 islands <- st_as_sf(c(st_geometry(polys), castpolys))
+
+# Basic island id
 islands$id <- seq.int(length.out = nrow(islands))
 
 
-
-
-# Convert to an sf object
-sfislands <- st_as_sf(
-  st_as_stars(islands),
-  as_points = FALSE,
-  use_integer = TRUE,
-  merge = TRUE,
-  na.rm = TRUE
-)
-sfislands$island <- seq_along(sfislands$geometry)
+### Reproject islands ----
+# Projections
+utm <- st_crs('+proj=utm +zone=21 ellps=WGS84')
 
 # Project to UTM
-utmbb <- projectExtent(raster(ext = extent(bb), crs = latlon$proj4string), 
-                       crs = utm)
-out <- fasterize(sfislands, utmbb, field = "island")
+utmislands <- st_transform(islands, utm)
+
+### Rasterize ----
+utmbb <- projectExtent(
+  object = raster(ext = extent(bb), 
+             crs = latlon$proj4string),
+  crs = utm
+)
+
+out <- fasterize(utmislands, utmbb, field = "island")
 
 ### Output ----
 saveRDS(sfislands, "output/islandsPoly.Rds")
