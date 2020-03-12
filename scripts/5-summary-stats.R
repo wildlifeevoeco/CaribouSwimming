4bd773b9b774578c31f86e31496eadfd91e0c86d
Alec L. Robitaille <robit.alec@gmail.com>
2020-03-12 18:14:38 -0230

    basic stats

--- scripts/5-summary-stats.R
+++ scripts/5-summary-stats.R
@@ -12,5 +12,19 @@ lapply(libs, require, character.only = TRUE)
 ### Data ----
 edges <- readRDS('output/island-edges.Rds')
 
+icefree <- edges[season == 'icefree']
 
-### 
+### Stats ----
+## Only icefree
+# Number of events
+icefree[, .N]
+
+# Number of individuals
+icefree[, uniqueN(ANIMAL_ID)]
+
+# Number of events by individual
+icefree[, .N, ANIMAL_ID]
+
+## Full
+# Number of events by individual + icefree season
+edges[, .N, .(ANIMAL_ID, season)]
