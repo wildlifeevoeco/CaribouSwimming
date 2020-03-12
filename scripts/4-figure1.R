4dc17ded45f9867cab900730d6a43185203a380a
Alec L. Robitaille <robit.alec@gmail.com>
2020-03-12 16:16:37 -0230

    mv old plots

--- scripts/4-figure1.R
+++ scripts/4-figure1.R
@@ -33,23 +33,23 @@ ggplot() + geom_sf(data = islands, fill = '#c7c0bd') +
 # geom_nodetext(data = net, aes(xisl, yisl, label = name))
 
 # By id
-(gnn <- (ggplot(data = edges) + 
-           geom_sf(data = islands, fill = '#c7c0bd'))) +  
-   ylim(edges[, min(NORTHING) - 1000], edges[, max(NORTHING) + 1000]) +
-   xlim(edges[, min(EASTING) - 1000], edges[, max(EASTING) + 1000]) +
-   geom_edges(aes(x = EASTING,
-                  y = NORTHING, 
-                  xend = endislandEAST,
-                  yend = endislandNORTH,
-                  color = ANIMAL_ID)
-   ) +
- guides(color = FALSE, fill = FALSE) + 
-   scale_shape_manual(values = c('TRUE' = 2, 'FALSE' = 4))+
- scale_color_viridis_d() +
-  theme(panel.background = element_rect(fill = '#d0dee5')) 
- # guides(color = FALSE, size = FALSE) +
- # geom_text(aes(x, y, xend = NULL, yend = NULL, label = label), data = labels) + 
- # p
+(gnn <- (ggplot(data = edges) +
+            geom_sf(data = islands, fill = '#c7c0bd')) +
+  ylim(edges[, min(NORTHING) - 1000], edges[, max(NORTHING) + 1000]) +
+  xlim(edges[, min(EASTING) - 1000], edges[, max(EASTING) + 1000]) +
+  geom_edges(aes(x = EASTING,
+                 y = NORTHING,
+                 xend = endislandEAST,
+                 yend = endislandNORTH,
+                 color = ANIMAL_ID)) +
+  # guides(color = FALSE, fill = FALSE) +
+  scale_shape_manual(values = c('TRUE' = 2, 'FALSE' = 4)) +
+  # scale_color_viridis_d() +
+    facet_wrap(~season) + 
+  theme(panel.background = element_rect(fill = '#d0dee5'))
+# guides(color = FALSE, size = FALSE) +
+# geom_text(aes(x, y, xend = NULL, yend = NULL, label = label), data = labels) +
+# p
 )
 
-  
+
