3541929090586287aaea9f690220348af432cc84
Alec L. Robitaille <robit.alec@gmail.com>
2020-07-16 18:50:14 -0230

    add areas, group size and calc cow

--- scripts/5-table1.R
+++ scripts/5-table1.R
@@ -13,10 +13,18 @@ tab1 <- data.table(
   movto = ,
   movfrom = ,
   proplic = ,
-  density = ,
-  grpsize = ,
-  calfcow = 
+  density = cI,
+  grpsize = c('3.05 (2.7, 3.4)',
+              '2.95 (1.89, 4.02)',
+              '3.71 (1.97, 5.46)'),
+  calfcow = c('0.34, (0.28, 0.39) (n = 283 groups)',
+              '0.22, (0.05, 0.39) (n = 23 groups)',
+              '0.67, (0.23, 1.00) (n = 7 groups)'),
+  area = c(255, 7.5, 3.8)
 )
+
+tab1[, density := density / area]
+tab1[, area := NULL]
 
 setnames(tab1,
          c('',
@@ -24,7 +32,7 @@ setnames(tab1,
            'Moves away',
            'Proportion lichen',
            'Estimated density (caribou per km2)',
-           'Average group size',
+           'Average group size (95% CI)',
            'Calf:cow ratio between June and August'))
 
 
