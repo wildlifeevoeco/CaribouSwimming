b2308ff0b5351a29180dc672143b5bd6a2ce5aca
Alec L. Robitaille <robit.alec@gmail.com>
2020-09-21 13:30:25 -0230

    update plan with tabs

--- scripts/plan.R
+++ scripts/plan.R
@@ -6,7 +6,7 @@ get_islands <- code_to_function('scripts/1-osm-islands.R')
 extract_island_locs <- code_to_function('scripts/2-extract-island-locs.R')
 generate_edges <- code_to_function('scripts/3-generate-edges.R')
 fig_2 <- code_to_function('scripts/4-figure2.R')
-table_1 <- code_to_function('scripts/5-table1.R')
+tables <- code_to_function('scripts/5-tables.R')
 render_md <- code_to_function('scripts/6-render-md.R')
 
 
@@ -16,6 +16,6 @@ plan <- drake_plan(
   locs = extract_island_locs(islands),
   edges = generate_edges(locs),
   fig2 = fig_2(edges),
-  tab1 = table_1(edges),
-  md = render_md(fig2, tab1)
+  tabs = tables(edges),
+  md = render_md(fig2, tabs)
 )
