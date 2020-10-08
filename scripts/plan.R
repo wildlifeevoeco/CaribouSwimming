7fe3c570cdb5a2ebc4ad9ae0f86785c54cb636fc
Alec L. Robitaille <robit.alec@gmail.com>
2020-10-08 14:19:20 -0230

    update plan, fig2 depends on tab

--- scripts/plan.R
+++ scripts/plan.R
@@ -5,8 +5,8 @@ library(drake)
 get_islands <- code_to_function('scripts/1-osm-islands.R')
 extract_island_locs <- code_to_function('scripts/2-extract-island-locs.R')
 generate_edges <- code_to_function('scripts/3-generate-edges.R')
-fig_2 <- code_to_function('scripts/4-figure2.R')
-tables <- code_to_function('scripts/5-tables.R')
+tables <- code_to_function('scripts/4-tables.R')
+fig_2 <- code_to_function('scripts/5-figure2.R')
 render_md <- code_to_function('scripts/6-render-md.R')
 
 
@@ -15,7 +15,7 @@ plan <- drake_plan(
   islands = get_islands(),
   locs = extract_island_locs(islands),
   edges = generate_edges(locs),
-  fig2 = fig_2(edges),
   tabs = tables(edges),
+  fig2 = fig_2(edges, tabs),
   md = render_md(fig2, tabs)
 )
