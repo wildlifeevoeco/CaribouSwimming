614c8ae3e9dee834145c44738e39144d60fe9cc1
Quinn Webber <webber.quinn@gmail.com>
2021-02-15 11:57:48 -0330

    distance fig

--- scripts/7-island-dist.R
+++ scripts/7-island-dist.R
@@ -18,3 +18,18 @@ dyad_id(edges, 'island', 'endisland')
 # Calculate mean step length between islands, undirected
 edges[, swimdist := mean(stepLength), dyadID]
 
+edges2 <- edges[, mean(swimdist), by = c("diff", "island")]
+edges2$swimdist <- edges2$V1/10
+
+## load area data
+runarea <- readRDS('output/runarea.Rds')
+
+df <- merge(edges2, runarea[, c("area", "island", "islandlen")], by = "island", allow.cartesian=TRUE)
+df$island <- as.character(df$island)
+
+df.complete <- df[complete.cases(df),]      
+      
+ggplot(df) +
+  geom_point(aes(log(islandlen/area), swimdist)) +
+  ylab("distance between islands") +
+  xlab("log(residency/area)")
