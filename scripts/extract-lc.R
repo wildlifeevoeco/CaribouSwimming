58abed513d9722216ec39716011255f64875ec3a
Quinn Webber <webber.quinn@gmail.com>
2021-05-20 14:20:12 -0230

    Update extract-lc.R

--- scripts/extract-lc.R
+++ scripts/extract-lc.R
@@ -1,3 +1,9 @@
+
+library(sf)
+library(rgdal)
+library(raster)
+library(data.table)
+
 extract_lc <- function(value, lc, poly) {
   if (is.na(value)) {
     extract(is.na(lc), poly, fun = mean)
@@ -5,7 +11,12 @@ extract_lc <- function(value, lc, poly) {
     extract(lc == value, poly, fun = mean, na.rm = TRUE)
   }
 }
+
+## load data
 fogopolys <- readRDS('../caribou-swimming/output/islandsPoly.Rds')
+fogolc <- raster('../fogo_coyote_repeat/data/raw-data/Landcover/FogoSDSS_RS.tif')
+
+## extract proportion of habitat type by island
 values <- c(NA, unique(fogolc))
 extracts <- lapply(values, extract_lc, fogolc, fogopolys)
 names(extracts) <- paste0('prop', values)
@@ -13,3 +24,16 @@ DT <- as.data.table(extracts)
 DT[, id := fogopolys$id]
 DT[, area := fogopolys$area]
 saveRDS(DT, '../caribou-swimming/output/fogo-prop-lc.Rds')
+
+## load data
+DT <- readRDS('../caribou-swimming/output/fogo-prop-lc.Rds')
+legend <- fread('../fogo_coyote_repeat/data/raw-data/Landcover/Legend.csv')
+
+DT[is.na(DT)] <- 0
+
+setnames(DT, c("prop1", "prop2", "prop3", "prop4",
+               "prop5", "prop6", "prop7", "prop8",
+               "prop9", "propNA"),
+         unique(legend$Cover))
+
+
