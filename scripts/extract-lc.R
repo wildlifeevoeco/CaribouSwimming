aefc102a259f10c717cd57d2af9e25c77da6e04a
Quinn Webber <webber.quinn@gmail.com>
2021-05-21 11:24:59 -0230

    Update extract-lc.R

--- scripts/extract-lc.R
+++ scripts/extract-lc.R
@@ -34,20 +34,13 @@ setnames(DT, c("prop1", "prop2", "prop3", "prop4",
                "prop9", "propNA"),
          unique(legend$Cover))
 
-## island ids
-## Fogo = 120
-## W. Perry = 128
-## E. Perry = 124
-## Long skinny island north of W. Perry = 75
-## Island east of W. Perry = 70
-## Island southwest of W. Perry = 125
-## Island south of W. Perry = 78
-## Island southcentre of W. Perry = 73
-
 DT <- DT[id == 120 | id == 128 | id == 124 |
          id == 75 | id == 70 | id == 125  |
-         id == 78 | id == 73]
+         id == 78 | id == 73 | id == 58 | id == 72]
 
+## change names of islands ids to island names 
+DT$id[DT$id == 58] <- "Island 6"
+DT$id[DT$id == 72] <- "Island 7"
 DT$id[DT$id == 70] <- "Island 1"
 DT$id[DT$id == 73] <- "Island 2"
 DT$id[DT$id == 75] <- "Island 3"
@@ -59,3 +52,5 @@ DT$id[DT$id == 128] <- "W. Perry"
 
 DT$other <- DT$Broadleaf +DT$MixedWood + DT$Anthro + DT$NotAvail
 DT$conifer <- DT$ConiferScrub + DT$ConiferForest
+
+DT$areakm2 <- DT$area/1000000
